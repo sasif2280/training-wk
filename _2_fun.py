1048f0455ec8038b635b6dfd2f9115fed7a90a45
Asif Shaikh <Asif.Shaikh@wkfs-frc.local>
2022-06-24 17:05:40 +0530

    first tasks

---
+++ _2_fun.py
@@ -0,0 +1,9 @@
+x=80   
+
+def add():
+    global x
+    b=20
+    print(x+b)
+
+
+add()
---
+++ demo.py
@@ -0,0 +1,8 @@
+name="asif"
+age=name
+print(id(age))
+print(id(name))
+name="karan"
+print(id(name))
+
+print(1,2,3,4)  #in python we can print multiple values at a time.
