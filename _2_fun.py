f9718816f90794b19129f3572550013d778cd11c
Asif Shaikh <Asif.Shaikh@wkfs-frc.local>
2022-06-25 10:13:55 +0530

    unwanted files deleted

--- _2_fun.py
+++
@@ -1,9 +0,0 @@
-x=80   
-
-def add():
-    global x
-    b=20
-    print(x+b)
-
-
-add()
--- demo.py
+++
@@ -1,8 +0,0 @@
-name="asif"
-age=name
-print(id(age))
-print(id(name))
-name="karan"
-print(id(name))
-
-print(1,2,3,4)  #in python we can print multiple values at a time.
