c6fc52ed6974a20623666ea24c74e724b272844e
asif shaikh <sasif2280@gmail.com>
2022-07-21 16:38:06 +0530

    changes applied

--- smallprojets/ceasorcypher.py
+++ smallprojets/ceasorcypher.py
@@ -1,17 +1,17 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-message_actual = input("enter the message: ").lower()
 
-cypher_key = 5
+def encrypt():
+    message_actual = input("enter the message: ").lower()
 
-
-def encrypt(message, key):
+    cypher_key = int(
+        input("enter cypher key ! (*****rember key must be lesser than 26******)"))
     encrypted_message = ""
 
-    for letter in message:
+    for letter in message_actual:
         position = alphabets.index(letter)
-        new_pos = position+key
+        new_pos = position+cypher_key
         encrypted_message += alphabets[new_pos]
 
     print(encrypted_message)
@@ -19,9 +19,9 @@ def encrypt(message, key):
     choice = input("do you want to decrypt the message ? yes / no :").lower()
 
     if choice == "yes":
-        decrypt(encrypted_message, key)
+        decrypt(encrypted_message, cypher_key)
     else:
-        pass
+        print("good bye !!")
 
 
 def decrypt(encrypted_message, key):
@@ -30,7 +30,40 @@ def decrypt(encrypted_message, key):
         position = alphabets.index(letter)
         new_pos = position-key
         decrypted_message += alphabets[new_pos]
-    print(f"decrypted message is {decrypted_message}")
+    print(f"decrypted message is {decrypted_message}\n \n")
+
+    choice = input("do you like to encrypt the message again ? :").lower()
+
+    if choice == "yes":
+        encrypt()
+    else:
+        print("good bye !!")
 
 
-encrypt(message_actual, cypher_key)
+def new_decrypt(encrypted_message, key):
+    decrypted_message = ""
+    for letter in encrypted_message:
+        position = alphabets.index(letter)
+        new_pos = position-key
+        decrypted_message += alphabets[new_pos]
+    print(f"decrypted message is {decrypted_message}\n \n")
+    choice = input("do you like to encrypt the message again ? :").lower()
+    if choice == "yes":
+        encrypt()
+    else:
+        print("good bye !!")
+
+
+def main_menu():
+    print("\t1 >> encrypt\n")
+    print("\t2 >> decrypt \n")
+    choice = int(input("select one option : "))
+
+    match(choice):
+        case 1:
+            encrypt()
+        case 2:
+            new_decrypt()
+
+
+main_menu()
